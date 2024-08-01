5df5660b85109c198c130a922f529da3c25f30dc
nikhil-polymath <nagasainikhil100@gmail.com>
2024-08-01 18:21:45 +0530

    Create cliparse.cs

---
+++ ADEnumeration/add-computer/cliparse.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_computer
+{
+    class cliparse
+    {
+
+        public string getargvalue(string[] args, string argkey)
+        {
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].ToLower().Equals(argkey.ToLower()))
+                {
+                    try
+                    {
+                        return args[i + 1];
+
+                    }
+                    catch (Exception e)
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
