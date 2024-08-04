58579d7a29a676ebaa8621e1000c100f119cf2f7
nikhil-polymath <nagasainikhil100@gmail.com>
2024-08-04 17:55:47 +0530

    Create Program.cs

---
+++ ADEnumeration/get-asreproastableusers/Program.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_asreproastable
+{
+    class Program
+    {
+        public static string username = null;
+        public static string password = null;
+
+        public static List<string> getuacvalues(int uacnumber)
+        {
+
+            List<string> l = new List<string>();
+
+            l.Add(""); l.Add("ACCOUNTDISABLE"); l.Add(""); l.Add("HOMEDIR_REQUIRED");
+            l.Add("LOCKOUT"); l.Add("PASSWD_NOTREQD"); l.Add("PASSWD_CANT_CHANGE");
+            l.Add("ENCRYPTED_TEXT_PWD_ALLOWED"); l.Add("TEMP_DUPLICATE_ACCOUNT");
+            l.Add("NORMAL_ACCOUNT"); l.Add(""); l.Add("INTERDOMAIN_TRUST_ACCOUNT");
+            l.Add("WORKSTATION_TRUST_ACCOUNT"); l.Add("SERVER_TRUST_ACCOUNT"); l.Add(""); l.Add("");
+            l.Add("DONT_EXPIRE_PASSWORD"); l.Add("MNS_LOGON_ACCOUNT");
+            l.Add("SMARTCARD_REQUIRED"); l.Add("TRUSTED_FOR_DELEGATION");
+            l.Add("NOT_DELEGATED"); l.Add("USE_DES_KEY_ONLY");
+            l.Add("DONT_REQ_PREAUTH"); l.Add("PASSWORD_EXPIRED");
+            l.Add("TRUSTED_TO_AUTH_FOR_DELEGATION");
+
+
+            string uac_binary = Convert.ToString(uacnumber, 2);
+            List<string> flags = new List<string>();
+            //Console.WriteLine(l.Count);
+            //Console.WriteLine(uac_binary.Length);
+            for (int i = 0; i < uac_binary.Length; i++)
+            {
+                int result2 = uacnumber & Convert.ToInt32(Math.Pow(2, i));
+                if (result2 != 0)
+                {
+                    //Console.WriteLine(l[i]);
+                    flags.Add(l[i]);
+                }
+
+            }
+
+            return flags;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            // get_asreproastable -sid SID  -domain domainname
+            // -server serverip
+            // -username username -password password
+
+            cliparse cli = new cliparse();
+
+
+
+            DirectoryEntry de;
+
+            string domainname = cli.getargvalue(args, "-domain");
+            string serverip = cli.getargvalue(args, "-server");
+            Program.username = cli.getargvalue(args, "-username");
+            Program.password = cli.getargvalue(args, "-password");
+
+            
+
+            if (domainname == null && serverip == null)
+            {
+                string domain = Domain.GetCurrentDomain().ToString();
+                string[] dcs = domain.Split('.');
+
+                for (int i = 0; i < dcs.Length; i++)
+                {
+                    dcs[i] = "DC=" + dcs[i];
+                    //Console.WriteLine(dcs[i]);
+                }
+                de = new DirectoryEntry(String.Format("LDAP://{0}", String.Join(",", dcs)));
+
+            }
+
+
+            else if (serverip != null)
+            {
+                de = new DirectoryEntry(String.Format("LDAP://{0}", serverip));
+            }
+
+            else
+            {
+                string[] dcs = domainname.Split('.');
+
+                for (int i = 0; i < dcs.Length; i++)
+                {
+                    dcs[i] = "DC=" + dcs[i];
+                    //Console.WriteLine(dcs[i]);
+                }
+                de = new DirectoryEntry(String.Format("LDAP://{0}", String.Join(",", dcs)));
+
+            }
+
+            if (Program.username != null && Program.password != null)
+            {
+                de.Username = Program.username;
+                de.Password = Program.password;
+            }
+
+            DirectorySearcher ds = new DirectorySearcher(de);
+            ds.Filter = "(useraccountcontrol>=4194303)";
+
+            foreach (SearchResult sr in ds.FindAll())
+            {
+                int uac = Convert.ToInt32(sr.Properties["useraccountcontrol"][0]);
+
+                List<string> uacstrings = getuacvalues(uac);
+                foreach (string uacstring in uacstrings)
+                {
+                    if (uacstring.ToUpper() == "DONT_REQ_PREAUTH")
+                    {
+                        Console.WriteLine("[+] {0}",sr.Properties["distinguishedname"][0]);
+                        Console.WriteLine();
+                    }
+                }
+
+            }
+
+        }
+    }
+}
